55a887c649f9b07d3663903fac12db7d867a269b
selcukcokkuvvetli <selcuk_cokkuvvetli@hotmail.com>
2026-06-05 13:55:35 +0300

    Add line description tooltips for endeks chart

---
+++ files/js/endeks-line-descriptions.js
@@ -0,0 +1,53 @@
+window.imsadEndeksLineDescriptions = {
+  "Bileşik Endeks": {
+    Endeks:
+      "İnşaat Malzemesi Sanayi Bileşik Endeksi ağustos ayında bir önceki aya göre 1,2 puan gerileyerek 79,8 puana inmiştir. Bileşik Endeks 2024 Ağustos ayında ise 89,2 puan olmuştu. Bileşik Endeks için yılın geri kalanında daha olumlu koşullar olabilecektir.",
+  },
+  "Faaliyet Endeksi": {
+    Endeks:
+      "Faaliyet Endeksi ağustos ayında bir önceki aya göre 2,3 puan azalmış ve 123,7 puana inmiştir. Faaliyet Endeksi 2024 yılı ağustos ayı seviyesinin de altında kapatmıştır.",
+    "Yurt İçi Satış Endeksi":
+      "Yurt içi satışlar ağustos ayında bir önceki aya göre düşmüştür.",
+    "Üretim Endeksi":
+      "Ağustos ayında üretimde belirgin bir düşüş gerçekleşmiştir.",
+    "İhracat Endeksi":
+      "Ağustos ayında ihracat bir önceki aya göre gerilemiştir.",
+    "Ciro Endeksi": "Ağustos ayında cirolarda düşüşler yaşanmıştır.",
+    "Tahsilat Hızı Endeksi":
+      "Tahsilat hızı ağustos ayında bir önceki aya göre gerilemiştir.",
+    "Yurt Dışı Satış Fiyatları Endeksi":
+      "Ağustos ayında yurt dışı satış fiyatları bir önceki aya göre sınırlı ölçüde düşmüştür.",
+  },
+  "Güven Endeksi": {
+    Endeks:
+      "Güven seviyesi ağustos ayında bir önceki aya göre 0,9 puan düşmüş ve 53,6 puan olarak gerçekleşmiştir. Güven Endeksi 2024 yılı ağustos ayını ise 65,2 puan seviyesinde kapatmıştı.",
+    "Türkiye Ekonomisi Genel Gidişat Endeksi":
+      "Türkiye ekonomisi genel gidişat endeksi ağustos ayında sınırlı ölçüde düşmüştür.",
+    "İnşaat Sektörü Genel Gidişat Endeksi":
+      "İnşaat sektörü genel gidişat endeksi ağustos ayında sınırlı şekilde azalmıştır.",
+    "İnşaat Malzemeleri Sanayi Genel Gidişat Endeksi":
+      "İnşaat malzemesi sanayisi genel gidişat endeksi ağustos ayında sınırlı ölçüde daralmıştır.",
+    "Yurt İçi Pazarlar Genel Gidişat Endeksi":
+      "Yurt içi pazarlar genel gidişat endeksi ağustos ayında sınırlı ölçüde düşmüştür.",
+    "İhracat Pazarları Genel Gidişat Endeksi":
+      "İhracat pazarları genel gidişat endeksi ağustos ayında sınırlı ölçüde gerilemiştir.",
+  },
+  "Beklenti Endeksi": {
+    Endeks:
+      "Beklenti Endeksi ağustos ayında bir önceki aya göre 0,6 puan azalmıştır. Beklenti Endeksi 65,4 puana inmiştir. Beklenti Endeksi 2024 yılı ağustos ayında ise 74,6 puan olmuştu.",
+    "Türkiye Ekonomisine İlişkin 3 Aylık Beklenti Endeksi":
+      "Türkiye ekonomisine ilişkin beklentiler ağustos ayında sınırlı ölçüde düşmüştür.",
+    "İnşaat Malzemesi Sanayine İlişkin 3 Aylık Beklenti Endeksi":
+      "İnşaat malzemesi sanayi beklentileri ağustos ayında sınırlı ölçüde azalmıştır.",
+    "Önümüzdeki 3 Aya İlişkin Alınan Yurt İçi Siparişler Endeksi":
+      "Alınan yurt içi siparişleri ağustos ayında sınırlı düşüş göstermiştir.",
+    "Önümüzdeki 3 Aya İlişkin Alınan İhracat Siparişleri Endeksi":
+      "Alınan ihracat siparişleri ağustos ayında yeniden gerilemiştir.",
+    "Önümüzdeki 3 Aya İlişkin Üretim Beklentisi Endeksi":
+      "Önümüzdeki üç aya ilişkin üretim beklentileri ağustos ayında artmıştır.",
+    "Önümüzdeki 3 Aya İlişkin Yatırım Beklentisi Endeksi":
+      "Yatırım beklentileri ağustos ayında sınırlı düşüş göstermiştir.",
+    "Önümüzdeki 3 Aya İlişkin İstihdam Beklentisi Endeksi":
+      "İstihdam beklentileri ağustos ayında gerileme göstermiştir.",
+  },
+};
